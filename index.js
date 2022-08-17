66cfc4155ca716b3acecee79e5921c5c71efa8bc
Sabrina Kramer <01CodePathways11@gmail.com>
2022-08-16 19:57:06 -0500

    Create files

---
+++ index.js
@@ -0,0 +1,15 @@
+//store gameboard as array inside of gameboard object
+//store players in objects
+//use object to control game flow
+
+//goal: have as little global code as possible
+//Rule of thumb: if need ONE of something, use a module. If need MULTIPLE of something, create with factories
+
+//set up HTML
+//write JS function to render contents of array (manually fill array for now)
+//make functions that allow players to mark to specific spot on board 
+//check for game over (3 in a row and tie)
+//clean up interface to allow players to add their names
+//include start/restart button
+//congratz for winning player
+//(create AI computer?)
